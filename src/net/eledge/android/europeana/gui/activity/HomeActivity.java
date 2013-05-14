fd9514a45cda055da973019b345bb40e9e778f2e
wjboogerd <wjboogerd@eledge.net>
2013-05-14 15:46:05 +0200

    Added Home activity

---
+++ src/net/eledge/android/europeana/gui/activity/HomeActivity.java
@@ -0,0 +1,23 @@
+package net.eledge.android.europeana.gui.activity;
+
+import net.eledge.android.europeana.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+}
---
+++ src/net/eledge/android/europeana/provider/SuggestionProvider.java
@@ -0,0 +1,9 @@
+package net.eledge.android.europeana.provider;
+
+import android.content.SearchRecentSuggestionsProvider;
+
+public class SuggestionProvider extends SearchRecentSuggestionsProvider {
+	
+	
+
+}
