ee85f1f5fcc44e39f66e592068bfc2064d7b0c9d
wjboogerd <wjboogerd@eledge.net>
2013-08-01 23:09:54 +0200

    Starting to define a Record from JSON

--- src/net/eledge/android/eu/europeana/search/model/record/Record.java
+++ src/net/eledge/android/eu/europeana/search/model/record/Record.java
@@ -1,12 +1,29 @@
 package net.eledge.android.eu.europeana.search.model.record;
 
 import net.eledge.android.eu.europeana.search.model.enums.DocType;
+import net.eledge.android.toolkit.json.annotations.JsonField;
 
 public class Record {
 	
-	public String id;
+	@JsonField
 	public String title = "Invalid title...";
+	
+	@JsonField
 	public DocType type;
-	public String thumbnail;
+	
+	@JsonField("proxies[].dcCreator.def")
+	public String[] dcCreator;
+	
+	@JsonField("proxies[].dcSubject.def")
+	public String[] dcSubject;
+	
+	@JsonField("europeanaAggregation[].edmPreview")
+	public String edmPreview;
+	
+	@JsonField("aggregations[].edmIsShownBy")
+	public String edmIsShownBy;
+	
+	@JsonField("aggregations[].edmIsShownAt")
+	public String edmIsShownAt;
 
 }
