0ecee7ee6f21c975f8d10dc282ba142278c0ac45
wjboogerd <wjboogerd@eledge.net>
2013-08-06 17:07:45 +0200

    Using the JsonParser...

--- src/net/eledge/android/eu/europeana/search/RecordController.java
+++ src/net/eledge/android/eu/europeana/search/RecordController.java
@@ -1,9 +1,18 @@
 package net.eledge.android.eu.europeana.search;
 
+import net.eledge.android.eu.europeana.search.model.record.Record;
+import net.eledge.android.toolkit.json.JsonParser;
+
 
 public class RecordController {
 
 	public final static RecordController instance = new RecordController();
+	
+	private final JsonParser<Record> jsonParser;
+	
+	public RecordController() {
+		jsonParser = new JsonParser<Record>(Record.class);
+	}
 	
 	public String getPortalUrl() {
 //		try {
