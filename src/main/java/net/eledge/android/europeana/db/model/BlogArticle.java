75e7a27cc805e365069f0172589d51a8aaae5425
Willem-Jan Boogerd <wjboogerd@eledge.net>
2015-02-26 15:14:28 +0100

    Add image to blog db object

--- src/main/java/net/eledge/android/europeana/db/model/BlogArticle.java
+++ src/main/java/net/eledge/android/europeana/db/model/BlogArticle.java
@@ -44,6 +44,9 @@ public class BlogArticle implements BaseColumns {
 
     @Column
     public String author;
+    
+    @Column
+    public string image;
 
     @Column
     public boolean markedNew;
